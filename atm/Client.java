e18db438a2f7fcf1a8dae5f27b789b8a6c352cee
Yoan Dzhelekarski <yoan.dzhelekarski@gmail.com>
2023-12-12 17:47:44 +0200

    reformat live code

--- atm/Client.java
+++ atm/Client.java
@@ -9,44 +9,32 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        // connecting to server on the localhost and port 1234
-        Socket socket = new Socket("127.0.0.1", 1234);
-        
-        Scanner scan = new Scanner(System.in);
+        try (
+            // connecting to server on the localhost and port 1234
+            Socket socket = new Socket("127.0.0.1", 1234);
+            
+            Scanner scan = new Scanner(System.in);
 
-        // creating readers and writers for communication with the server
-        OutputStream out = socket.getOutputStream();
-        InputStream in = socket.getInputStream();
-        Scanner reader = new Scanner(in);
-        PrintStream writer = new PrintStream(out);
+            // creating readers and writers for communication with the server
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+            Scanner reader = new Scanner(in);
+            PrintStream writer = new PrintStream(out);
+        ){
+            var text = reader.nextLine(); // read data from the server
+            System.out.println(text);
+            var bankNumber = scan.nextInt();
+            writer.println(bankNumber);
+            
+            do {
+                text = reader.nextLine();
+                System.out.println(text);
+            } while (!text.contains("Enter"));
 
-        //////////////////////////
-
-        var text = reader.nextLine(); // read data from the server
-        System.out.println(text);
-        var bankNumber = scan.nextInt();
-        writer.println(bankNumber);
-        while (reader.hasNextLine()) {
-            System.out.println("1");
+            var pin = scan.nextInt();
+            writer.println(pin);
             text = reader.nextLine();
             System.out.println(text);
-            if (text.contains("Enter")) {
-                break;
-            }
         }
-        var pin = scan.nextInt();
-        writer.println(pin);
-        text = reader.nextLine();
-        System.out.println(text);
-
-        //////////////////////////
-
-        reader.close();
-        writer.close();
-        socket.close();
-        scan.close();
-
-        // server_scan.close();
-        // socket.close();
     }
 }
--- atm/Server.java
+++ atm/Server.java
@@ -16,60 +16,48 @@ public class Server {
         bankAccounts.add(new Account(1002, 10, 1234));
         bankAccounts.add(new Account(1003, 100, 1234));
         bankAccounts.add(new Account(1004, 1000, 1234));
-        // creating server connected to TCP port 1234
-        ServerSocket server = new ServerSocket(1234);
-        // listening for new connections
-        Socket socket = server.accept();
 
-        // creating input and output streams
-        OutputStream out = socket.getOutputStream();
-        InputStream in = socket.getInputStream();
-        Scanner reader = new Scanner(in);
-        PrintStream writer = new PrintStream(out);
-    
-        //////////////////////////
+        try (
+            // creating server connected to TCP port 1234
+            ServerSocket server = new ServerSocket(1234);
+            // listening for new connections
+            Socket socket = server.accept();
 
-        writer.println("Welcome! Enter account number: "); // send data to client
-        var bankNumber = reader.nextInt();
-        System.out.println(bankNumber);
-        Account accountFound = null;
-        for (var bankAccount : bankAccounts) {
-            if (bankAccount.getAccountNumber() == bankNumber) {
-                accountFound = bankAccount;
-                writer.println("Bank account found with that number!");
-                break;
+            // creating input and output streams
+            OutputStream out = socket.getOutputStream();
+            InputStream in = socket.getInputStream();
+            Scanner reader = new Scanner(in);
+            PrintStream writer = new PrintStream(out);
+        ){
+            writer.println("Welcome! Enter account number: "); // send message to client
+            var bankNumber = reader.nextInt(); // receive number from client
+            System.out.println(bankNumber);
+            
+            // search for bank account
+            Account accountFound = null;
+            for (var bankAccount : bankAccounts) {
+                if (bankAccount.getAccountNumber() == bankNumber) {
+                    accountFound = bankAccount;
+                    writer.println("Bank account found with that number!");
+                    break;
+                }
+            }
+            if (accountFound == null) {
+                writer.println("No bank account found with that number! Bye!");
+                return;
+            }
+
+            writer.println("Enter pin code: "); // send message to client
+            var pin = reader.nextInt(); // receive number from client
+
+            // check pin
+            if (accountFound.checkPin(pin)) {
+                writer.println("PIN is correct!");
+            }
+            else {
+                writer.println("Try again!");
+                return;
             }
         }
-        if (accountFound == null) {
-            writer.println("No bank account found with that number! Bye!");
-            server.close();
-            return;
-        }
-
-        writer.println("Enter pin code: ");
-        var pin = reader.nextInt();
-        if (accountFound.checkPin(pin)) {
-            writer.println("PIN is correct!");
-        }
-        else {
-            writer.println("Try again!");
-            socket.close();
-            return;
-        }
-
-        //////////////////////////
-
-        in.close();
-        out.close();
-        reader.close();
-        writer.close();
-        socket.close();
-        server.close();
-        
-        // creating stream for sending data to client
-        // PrintWriter client_out = new PrintWriter(socket.getOutputStream());
-        // client_out.println("hi"); // send "hi" to the client
-
-        // server.close();
     }
 }
